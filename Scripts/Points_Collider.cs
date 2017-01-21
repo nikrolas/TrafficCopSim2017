18ff27021f228ea60ddb124d45240ffeacde167c
Kris <kris.delacruz@incognito.com>
2017-01-21 15:23:34 -0800

    Renamed Count to Score

--- Scripts/Points_Collider.cs
+++ Scripts/Points_Collider.cs
@@ -7,14 +7,12 @@ public class Points_Collider : MonoBehaviour {
     public int score;
     public Text scoreText;
 
-	// Use this for initialization
 	void Start ()
     {
         score = 0;
         getScoreText();
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
 		
@@ -32,6 +30,6 @@ public class Points_Collider : MonoBehaviour {
 
     void getScoreText()
     {
-        scoreText.text = "Count: " + score.ToString();
+        scoreText.text = "Score: " + score.ToString();
     }
 }
