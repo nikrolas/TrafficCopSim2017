33cd08f9f1b8317648c944042d86b7cfe9e0f976
Kris <kris.delacruz@incognito.com>
2017-01-21 15:12:47 -0800

    Added unfinished collider script for score

--- Scripts/Points_Collider.cs
+++ Scripts/Points_Collider.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Points_Collider : MonoBehaviour {
 
+
+    public int score;
+
+    private Rigidbody2D rb2d;
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+        rb2d = GetComponent<Rigidbody2D>();
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter(Collider2D coll) {
+
+        if(coll.CompareTag("")) {
+            score += 1;
+        }
+
+        if(coll.CompareTag(""))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
 }
