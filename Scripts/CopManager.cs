a942fda76e7166dd2224fb3746f4c168c8f474ba
Kris <kris.delacruz@incognito.com>
2017-01-21 19:44:07 -0800

    Adding Animation and Script for Cop

---
+++ Scripts/CopManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopManager : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			anim.SetInteger ("State", 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetInteger ("State", 1);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			anim.SetInteger ("State", 2);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			anim.SetInteger ("State", 3);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			anim.SetInteger ("State", 4);
+		}
+	}
+}
--- Scripts/Points_Collider.cs
+++ Scripts/Points_Collider.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Points_Collider : MonoBehaviour {
+
+
     public int score;
     public Text scoreText;
+
+    public BoxCollider2D topRoad;
+    public BoxCollider2D bottomRoad;
+    public BoxCollider2D leftRoad;
+    public BoxCollider2D rightRoad;
+
 
 	void Start ()
     {
@@ -20,7 +28,7 @@ public class Points_Collider : MonoBehaviour {
 
     void OnTriggerEnter(Collider2D coll)
     {
-        if (coll.CompareTag("")) {
+        if (coll.gameObject.name == "temp_car") {
             score += 1;
             getScoreText();
             coll.gameObject.SetActive(false);
